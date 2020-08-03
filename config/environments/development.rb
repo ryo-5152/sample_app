8f08986146dfd1eb44adeb5e901fa071b3c1a902
Nakamura Ryo <n.ryo.5152@icloud.com>
2020-08-03 15:14:06 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,11 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+ 
+
+  host = 'ap-northeast-1.console.aws.amazon.com/cloud9/ide/b1f1cae652d5420bad3f2566ed5f005f'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
