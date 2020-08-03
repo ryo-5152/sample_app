40d5b8e34c9e98f1ebd67d38efb1c72f85423bf2
Nakamura Ryo <n.ryo.5152@icloud.com>
2020-08-03 06:20:32 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,25 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
-    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
-  
- private
+
+  private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
